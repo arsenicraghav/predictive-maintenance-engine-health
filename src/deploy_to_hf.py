7b1b56b12a97bb624436c450b99cc1c2706979f2
arsenicraghav <write.to.raghav@outlook.com>
2025-11-16 10:54:19 +0000

    Commit from Colab: Create deploy_to_hf.py – hosting script to push deployment files to HF Space

---
+++ src/deploy_to_hf.py
@@ -0,0 +1,17 @@
+from huggingface_hub import HfApi
+import os
+
+def upload_space():
+    api = HfApi()
+    space_repo_id = "labhara/predictive-maintenance-app"  # update with your HF Space ID
+
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+
+    api.upload_folder(
+        folder_path=folder_path,
+        repo_id=space_repo_id,
+        repo_type="space"
+    )
+
+if __name__ == "__main__":
+    upload_space()
